1c122e5d8b60d30f2dffd614e7fe1521174ed470
robertsilen <robert.silen@iki.fi>
2017-09-27 15:20:51 +0300

    added Friedman & more

--- 3plusgrouptest.py
+++ 3plusgrouptest.py
@@ -2,10 +2,6 @@
 # By Robert Silen, robert.silen@iki.fi
 #
 # Reads cvs file, performs tests, and writes separate output csv file
-# Tests run: 
-# Kruskal-Wallis H Test for three or more independent groups of values 
-#  (prefered over "one-way ANOVA on ranks" when samples are not normaly distributed)
-# Basic stats on the value groups: variance, skew, kurtosis, etc. 
 #
 # To run script, write in command prompt or terminal: 
 # python 3plusgrouptest.py <name of input csv file>
@@ -15,40 +11,68 @@
 import sys
 import time
 import pandas as pd
+import numpy as np
 from scipy.stats import kruskal
-success = False
+from scipy.stats import friedmanchisquare
+from scipy.stats import levene
+from scipy.stats import shapiro
 
 # Read data from file and prepare for test
-test = 'kruskal'
-input = sys.argv[1]
-print("Reading data from file: ")
-print(input)
+input = 'example.csv'
+if sys.argv[1:]:
+	input = sys.argv[1]
+print("Reading: "+str(input))
 df = pd.read_csv(input)
 df.set_index('Person',inplace=True)
 df = df.T
 print('Dataframe size: '+str(df.shape)) 
-
-# Perform Kruskal and related tests
-if test=='kruskal': 
-	print("Performing Kruskal and related tests.")
-	results_zeroes = pd.DataFrame(columns = ['Zeroes'])
-	results_kruskal = pd.DataFrame(columns = ['Kruskal Static','Kruskal P-Value'])
 	
-	grouped = df[['Group']].groupby('Group')
-	for column in df.ix[:,1:]:
-		args = []
-		for name,group in grouped:
-			args.append(df[column].loc[df['Group'] == name].apply(pd.to_numeric))
+grouped = df[['Group']].groupby('Group')
+a = grouped.size().tolist()
+len_first = a[0] if a else None
+if all(x == len_first for x in a):
+	print ('Groups are equal size.')
+	equalsize = True
+else: 
+	print ('Groups are NOT equal size. Friedman test will not be run.')
+	equalsize = False
+	
+# Prepare to capture results
+results_nans = pd.DataFrame(columns = ['NaNs'])
+results_zeroes = pd.DataFrame(columns = ['Zeroes'])
+results_kurt = pd.DataFrame(columns = ['KURT-0', 'KURT-1'])
+results_skew = pd.DataFrame(columns = ['SKEW-0', 'SKEW-1'])
+results_var = pd.DataFrame(columns = ['VAR-0', 'VAR-1'])
+results_shapiro = pd.DataFrame(columns = ['Shapiro Norm-0', 'Shapiro Norm-1'])
+results_levene = pd.DataFrame(columns = ['Levene Static', 'Levene P Var'])
+results_kruskal = pd.DataFrame(columns = ['Kruskal Static','Kruskal P-Value'])
+results_friedman = pd.DataFrame(columns = ['Friedman Static','Friedman P-Value'])
+
+for column in df.ix[:,1:]:
+	results_zeroes.loc[column] = sum(df[column].apply(pd.to_numeric) == 0)
+	results_nans.loc[column] = sum(df[column].isnull())
+	args = []
+	for name,group in grouped:
+		args.append(df[column].loc[df['Group'] == name].apply(pd.to_numeric))
+	for i, element in enumerate(args):
+		results_kurt.loc[column,'KURT-'+str(i)] = element.kurt()
+		results_skew.loc[column,'SKEW-'+str(i)] = element.skew()
+		results_var.loc[column,'VAR-'+str(i)] = element.var()
+		results_shapiro.loc[column,'Shapiro Norm-'+str(i)] = shapiro(element)[1]
+	if df[column].isnull().values.any():
+		print('NaN values found: '+str(column))
+	else:
 		results_kruskal.loc[column] = (kruskal(args[0],*args[1:]))
-		results_zeroes.loc[column] = sum(df[column].apply(pd.to_numeric) == 0)
-	df = pd.concat([results_zeroes, results_kruskal, df.T], axis=1)
-	success = True
+		results_levene.loc[column] = (levene(args[0],*args[1:]))
+	if equalsize:
+		results_friedman.loc[column] = (friedmanchisquare(args[0],*args[1:]))	
+
+df = pd.concat([results_nans, results_zeroes, results_kurt, results_skew, results_var, results_shapiro, results_levene, results_kruskal, results_friedman, df.T], axis=1)
+
 
 # Write file
-if success: 
-	output = input+'-'+test+'-'+time.strftime("%d-%m-%Y")+".csv"
-	print("Writing data to file:")
-	print(output)
-	df.to_csv(output)   
-else:
-	print("Test not completed")
+if input.endswith('.csv'):
+    input = input[:-4]
+output = input+'_'+time.strftime("%d-%m-%Y")+".csv"
+print("Writing: "+str(output))
+df.to_csv(output)   
